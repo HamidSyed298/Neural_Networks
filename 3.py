d2ee39ceae0f813980c11e66b5f9cdfd64bf2a51
Hamid Habib Syed <122344493+HamidSyed298@users.noreply.github.com>
2026-06-10 10:48:33 -0500

    Refactor

--- 3.py
+++ 3.py
@@ -1,10 +1,10 @@
 import tensorflow as td
 from tensorflow import keras
-import numpy as numpy
+import numpy as np
+
 data = keras.datasets.imdb
 
 (train_data, train_labels), (test_data, test_labels) = data.load_data(num_words=88000)
-
 
 word_index = data.get_word_index()
 
